a1042c20bbfa349bfa2ae87747d3c7dc62ff65db
Gerben Welter <gerben.welter@asr.nl>
2016-09-16 13:46:30 +0200

    Use correct policy name

--- lib/puppet_x/lsp/security_policy.rb
+++ lib/puppet_x/lsp/security_policy.rb
@@ -915,7 +915,7 @@ class SecurityPolicy
                 :policy_type => "Registry Values",
                 :reg_type => "4"
             },
-            'System objects: Strengthen default permissions of internal system objects' => {
+            'System objects: Strengthen default permissions of internal system objects (e.g., Symbolic Links)' => {
                 :name => 'MACHINE\System\CurrentControlSet\Control\Session Manager\ProtectionMode',
                 :policy_type => "Registry Values",
                 :reg_type => "4"
