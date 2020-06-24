01a41e360eeedaf18214ea13c2fa04a5c9b7126b
Andy Adrian <andy.adrian@onyxpoint.com>
2020-06-24 14:58:00 -0500

    "Validate method failed" error #86
    Fixed ArgumentError syntax
    Fixed some rubocop compliants
    
    closes #86

--- lib/puppet/type/local_security_policy.rb
+++ lib/puppet/type/local_security_policy.rb
@@ -1,7 +1,9 @@
-#encoding: UTF-8
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 begin
-  require "puppet_x/lsp/security_policy"
-rescue LoadError => detail
+  require 'puppet_x/lsp/security_policy'
+rescue LoadError => e
   require 'pathname' # JJM WORK_AROUND #14073
   mod = Puppet::Module.find('local_security_policy', Puppet[:environment].to_s)
   if mod
@@ -25,7 +27,7 @@ Puppet::Type.newtype(:local_security_policy) do
   end
 
   newproperty(:policy_type) do
-    newvalues('System Access','Event Audit','Privilege Rights','Registry Values', nil, '')
+    newvalues('System Access', 'Event Audit', 'Privilege Rights', 'Registry Values', nil, '')
     desc 'Local Security Policy Machine Name.  What OS knows it by.'
     defaultto do
       begin
@@ -70,43 +72,41 @@ Puppet::Type.newtype(:local_security_policy) do
   newproperty(:policy_value) do
     desc 'Local Security Policy Setting Value'
     validate do |value|
-      if value.nil? or value.empty?
-        raise ArgumentError("Value cannot be nil or empty")
+      if value.nil? || value.empty?
+        raise ArgumentError, "Value cannot be nil or empty"
       end
-      case resource[:policy_type].to_s
-        when 'Privilege Rights'
-          # maybe validate some sort of user?
-        when 'Event Audit'
-          raise ArgumentError("Invalid Event type: #{value} for #{resource[:policy_value]}") unless SecurityPolicy::EVENT_TYPES.include?(value)
-        when 'Registry Values'
-          # maybe validate the value based on the datatype?
 
-          # REG_NONE 0
-          # REG_SZ 1
-          # REG_EXPAND_SZ 2
-          # REG_BINARY 3
-          # REG_DWORD 4
-          # REG_DWORD_LITTLE_ENDIAN 4
-          # REG_DWORD_BIG_ENDIAN 5
-          # REG_LINK 6
-          # REG_MULTI_SZ 7
-          # REG_RESOURCE_LIST 8
-          # REG_FULL_RESOURCE_DESCRIPTOR 9
-          # REG_RESOURCE_REQUIREMENTS_LIST 10
-          # REG_QWORD 11
-          # REG_QWORD_LITTLE_ENDIAN 11
+      case resource[:policy_type].to_s
+      when 'Privilege Rights'
+        # maybe validate some sort of user?
+      when 'Event Audit'
+        raise ArgumentError, "Invalid Event type: #{value} for #{resource[:policy_value]}" unless SecurityPolicy::EVENT_TYPES.include?(value)
+      when 'Registry Values'
+        # maybe validate the value based on the datatype?
+        # REG_NONE 0
+        # REG_SZ 1
+        # REG_EXPAND_SZ 2
+        # REG_BINARY 3
+        # REG_DWORD 4
+        # REG_DWORD_LITTLE_ENDIAN 4
+        # REG_DWORD_BIG_ENDIAN 5
+        # REG_LINK 6
+        # REG_MULTI_SZ 7
+        # REG_RESOURCE_LIST 8
+        # REG_FULL_RESOURCE_DESCRIPTOR 9
+        # REG_RESOURCE_REQUIREMENTS_LIST 10
+        # REG_QWORD 11
+        # REG_QWORD_LITTLE_ENDIAN 11
       end
     end
 
-    munge do | value |
+    munge do |value|
       # need to convert policy values to designated types
       case resource[:policy_type].to_s
-        when 'Registry Values'
-          # secedit values sometimes look like : "1,\"4\""
+      when 'Registry Values'
+        # secedit values sometimes look like : "1,\"4\""
       end
       SecurityPolicy.convert_policy_value(resource.to_hash, value)
     end
   end
 end
-
-
